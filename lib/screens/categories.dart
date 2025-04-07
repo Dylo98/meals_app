40a785bfdcda807f96b8f133d28d4a859550f9a4
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2025-04-07 16:41:37 +0200

    Add meal item to category

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/screens/meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Category Title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -37,7 +41,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectedCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
